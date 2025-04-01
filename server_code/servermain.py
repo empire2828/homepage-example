c732551841569c1b514130ab84f7f1c2beec093c
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-01 15:14:08 +0000

    Edited dashboard, settings, smoobu and servermain

--- server_code/servermain.py
+++ server_code/servermain.py
@@ -7,16 +7,13 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import google_linkedin
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+@anvil.server.callable
+def get_bookings_risk():
+    bookings = app_tables.bookings.search(email=anvil.users.get_user()['email'])
+    for booking in bookings:
+        bookings['screener_google_linkedin'] = google_linkedin(bookings['screener_google_linkedin'])
+        print(bookings['screener_google_linkedin'])
+    return bookings
+
