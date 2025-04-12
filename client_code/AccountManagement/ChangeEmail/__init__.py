119ca6e56d05fae447bec4a675f1094cce387ee9
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-12 15:42:43 +0000

    Edited AccountManagement.ChangeEmail, dashboard, guestscreen, homepage and 3 more

--- client_code/AccountManagement/ChangeEmail/__init__.py
+++ client_code/AccountManagement/ChangeEmail/__init__.py
@@ -6,7 +6,6 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class ChangeEmail(ChangeEmailTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
--- client_code/main/__init__.py
+++ client_code/main/__init__.py
@@ -2,7 +2,6 @@ from ._anvil_designer import mainTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
