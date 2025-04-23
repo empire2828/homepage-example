d12274ff613e89a878d7c43b16580359a8e9f6f9
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-23 21:12:43 +0000

    Edited dashboard

--- client_code/dashboard/__init__.py
+++ client_code/dashboard/__init__.py
@@ -4,7 +4,7 @@ import anvil.google.auth, anvil.google.drive
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
+from anvil import users
 import anvil.server
 
 class dashboard(dashboardTemplate):
@@ -28,11 +28,13 @@ class dashboard(dashboardTemplate):
     pass
 
   def resync_smoobu_button_click(self, **event_args):
-    alert("Hintergrund- Synchronisation wird gestartet. Das dauert ca. 10 Minuten.")
-    anvil.server.call_s('delete_bookings_by_email',anvil.users.get_user()['email'])
-    anvil.server.call_s('launch_sync_smoobu')
-    anvil.server.call_s('launch_get_bookings_risk')
-    self.init_components()
+    user = users.get_user()
+    if user[pms_api_key] != None:
+      alert("Hintergrund- Synchronisation wird gestartet. Das dauert ca. 10 Minuten.")
+      anvil.server.call_s('delete_bookings_by_email',anvil.users.get_user()['email'])
+      anvil.server.call_s('launch_sync_smoobu')
+      anvil.server.call_s('launch_get_bookings_risk')
+      self.init_components()
     pass
 
   def refresh_button_click(self, **event_args):
