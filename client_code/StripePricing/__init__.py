7302816d8fb62c69fbdd8f0e1f9ab9050714a0f3
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-25 14:42:04 +0000

    Edited StripePricing

--- client_code/StripePricing/__init__.py
+++ client_code/StripePricing/__init__.py
@@ -12,7 +12,26 @@ class StripePricing(StripePricingTemplate):
     self.init_components(**properties)
 
     #Any code you write here will run before the form opens.
-    pricing_table = self.dom_nodes['stripe-pricing-table']
+    # pricing_table = self.dom_nodes['stripe-pricing-table']
 
     # Passes the user's email to the Stripe checkout. This ensures records match in both Stripe and the app.
-    pricing_table.setAttribute("customer-email", anvil.users.get_user()["email"])
+    # pricing_table.setAttribute("customer-email", anvil.users.get_user()["email"])
+
+@anvil.server.callable
+def create_stripe_subscription(price_id, apartment_count):
+    user = anvil.users.get_user()
+    stripe_customer = anvil.stripe.get_customer(user['stripe_id'])  # Get existing customer
+  
+    # Calculate quantity based on apartment_count
+    subscription = anvil.stripe.Subscription.create(
+        customer=stripe_customer['id'],
+        items=[{
+            'price': price_id,
+            'quantity': apartment_count  # Pass apartment_count as quantity
+        }],
+        payment_behavior='default_incomplete',
+        expand=['latest_invoice.payment_intent']
+    )
+    return subscription
+
+
