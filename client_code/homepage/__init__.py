eaaea48c31cebc3fde478feaa515bfdb3ec69249
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-25 20:17:23 +0000

    Edited homepage and data_protection

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -6,9 +6,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from anvil_extras import routing
 
-@routing.route('data_protection')
 class homepage(homepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
