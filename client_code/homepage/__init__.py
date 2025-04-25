d8c254158f8c929bb7ec2955a58b24f1a8163f88
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-04-25 19:52:28 +0000

    Edited homepage

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -47,9 +47,10 @@ class homepage(homepageTemplate):
     ]
     )
     if result=='YES':
-      user = anvil.users.signup_with_form(allow_cancel=True)
-      if user:
-        open_form('dashboard')
+      #user = 
+      anvil.users.signup_with_form(allow_cancel=True)
+      #if user:
+      #  open_form('dashboard')
     pass
 
   def impressum_link_click(self, **event_args):
