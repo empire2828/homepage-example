67c414b7186d7f8da3378a2674f6a24034f465ea
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-05-20 17:55:43 +0000

    Edited dashboard, guestscreen and homepage

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -55,7 +55,3 @@ class homepage(homepageTemplate):
   def home_link_click(self, **event_args):
     open_form('homepage')
     pass
-
-  def second_test_button_click(self, **event_args):
-    self.testen_button_click()
-    pass
