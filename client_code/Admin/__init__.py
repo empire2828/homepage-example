f1ba7dab85441619723217485cfbd49badc82125
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-05-26 15:02:52 +0000

    Created Admin

---
+++ client_code/Admin/__init__.py
@@ -0,0 +1,22 @@
+from ._anvil_designer import AdminTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Admin(AdminTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def admin_file_loader_change(self, files, **event_args):
+    for f in files:
+      filedata = f.get_bytes()
+      anvil.server.call('read_csv', filedata)
+    pass
