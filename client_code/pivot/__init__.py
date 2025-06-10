87eeecfcb8fa207db1184d9a388911888f275e25
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-06-10 15:34:23 +0000

    Edited pivot

--- client_code/pivot/__init__.py
+++ client_code/pivot/__init__.py
@@ -7,14 +7,27 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from plotly import graph_objects as go
 
 class pivot(pivotTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+  
+    # Get your data from the server
     pivot_data = anvil.server.call('get_dashboard_data_dict')
-    self.pivot_1.items= pivot_data
-    self.pivot_1.rows = ['user']       # Group by user
-    self.pivot_1.columns = ['channel']    # Group by date
-    self.pivot_1.values = ['price']    # Summarize price
-    # Any code you write here will run before the form opens.
+    bookings = pivot_data['bookings']
+  
+    # Extract data for plotting
+    channel_names = [booking['channel_name'] for booking in bookings if booking['channel_name']]
+    adults_count = [booking['adults'] for booking in bookings if booking['adults']]
+  
+    # Create a bar chart
+    self.plot_1.data = go.Bar(x=channel_names, y=adults_count)
+  
+    # Optional: Configure layout
+    self.plot_1.layout = {
+      'title': 'Adults by Channel',
+      'xaxis': {'title': 'Channel'},
+      'yaxis': {'title': 'Number of Adults'}
+    }
