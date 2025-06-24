082a2bd99ecb2942e039aae7abe9d8d29dd8ec2b
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-06-24 19:12:37 +0000

    Created layout_template; Edited layout and analytics

--- client_code/layout_template/__init__.py
+++ client_code/layout_template/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import analyticsTemplate
+from ._anvil_designer import layout_templateTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class analytics(analyticsTemplate):
+class layout_template(layout_templateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
