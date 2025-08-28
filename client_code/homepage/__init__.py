a606e011414c0fe0e8df4df680eddd2629d09f48
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-08-28 20:52:36 +0000

    Edited homepage; Deleted homepage.faq_template

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -6,20 +6,6 @@ import anvil.users
 class homepage(homepageTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.faq_repeating_panel.items = [
-    {"question": "Was genau prüft die KI Suche?", "answer": "Die Suche sucht im Netz nach frei verfügbaren Informationen Ihrer Gäste und fasst diese zusammen. Gibt es mehrere Personen mit dem gleich oder ähnlichen Namen, kann die KI fehlerhafte Ergebnisse ausgeben."},
-    {"question": "Was genau prüft die Adress- und Telefonprüfung?", "answer": "Die Adresse wird daraufhin geprüft, ob es diese überhaupt gibt. Es wird nicht geprüft, ob der Gast tatsächlich hier wohnt. Bei der Telefonnummer wird auf formale Richtigkeit geprüft, nicht ob der Gast diese konkrete Telefonnummer oder jemand Anderes diese hat."},
-    {"question": "Wie erhält Lodginia.com die Informationen über meine Gäste?", "answer": "Sie können Lodginia.com per API direkt an Smoobu anbinden. Alternativ können Sie auch die Daten von einzelnen Gästen manuell eingeben."},
-    {"question": "Wie bekommen Sie die Informationen zurück?", "answer": "Sobald eine neue Buchung in Smoobu erfasst wird, erhalten Sie automatisch eine eMail mit den Informationen. Alternativ können Sie alles im Dashboard hier sehen."},
-    {"question": "Ist das Datenschutzkonform?", "answer": 
-      "Lodginia.com prüft Gästedaten für Ferienimmobilienvermieter auf Basis der DSGVO, da ein *berechtigtes Interesse* gemäß Art. 6 Abs. 1 lit. f DSGVO besteht: "
-      "Der Vermieter darf personenbezogene Daten verarbeiten, wenn dies notwendig ist, um das Eigentum vor Schäden oder Betrug zu schützen – etwa durch eine Risikoprüfung potenzieller Gäste. "
-      "Die Prüfung erfolgt ausschließlich anhand öffentlich zugänglicher Informationen, es werden keine sensiblen oder nicht relevanten Daten erhoben."
-      "\n\n"
-      "Die Datenverarbeitung ist auf das notwendige Maß beschränkt (Datenminimierung) und erfolgt nur für den klar definierten Zweck der Gefahrenabwehr (Zweckbindung). "
-      "14 Tage nach Ablauf der Buchung werden die Daten automatisiert gelöscht."
-      }
-    ]
 
   def login_button_click(self, **event_args):
     user = anvil.users.login_with_form(allow_cancel=True, show_signup_option=False, allow_remembered=True)
@@ -62,5 +48,9 @@ class homepage(homepageTemplate):
 
   def home_link_click(self, **event_args):
     open_form('homepage')
+    pass
+
+  def pricing_link_click(self, **event_args):
+    self.pricing_label.scroll_into_view()
     pass
 
