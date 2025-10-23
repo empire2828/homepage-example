4dc7fd751a967869109f1b4d4015ee0b94b013c5
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-10-23 16:13:03 +0000

    Edited impressum and help

--- client_code/help/__init__.py
+++ client_code/help/__init__.py
@@ -13,18 +13,7 @@ class help(helpTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.help_text_area.height=128
     # Any code you write here will run before the form opens.
-
-  def help_send_button_click(self, **event_args):
-    text=self.help_text_area.text
-    file=self.help_file_loader.file
-    user = anvil.users.get_user()
-    if user is not None:
-      email = user['email']
-    anvil.server.call('send_email_to_support',text, file, email)
-    alert('eMail wurde gesendet.')
-    pass
 
   def resync_button_click(self, **event_args):
     user = anvil.users.get_user()
@@ -44,3 +33,7 @@ class help(helpTemplate):
       else:
         # Code ausführen, wenn Benutzer Nein wählt oder das Popup schließt
         pass    
+
+  def link_1_click(self, **event_args):
+    """This method is called clicked"""
+    pass
