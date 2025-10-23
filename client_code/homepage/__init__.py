b9f4d44a2d51dbafb52958f2e71f3581e4749d35
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-10-23 12:07:55 +0000

    Edited settings and homepage

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -19,11 +19,13 @@ class homepage(homepageTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
     
-    Translations.set_dictionary('GE', GERMAN_LOCALE)
-    Translations.register_translation(self.headline_1, 'text')
-    Translations.register_translation(self.headline_1_2, 'text')
     user_lang = anvil.js.call_js('get_user_language')
-    Translations.set_locale(user_lang)
+    print(user_lang)
+    if user_lang and not user_lang.startswith("en"):
+      Translations.set_dictionary('GE', GERMAN_LOCALE)
+      Translations.register_translation(self.headline_1, 'text')
+      Translations.register_translation(self.headline_1_2, 'text')
+      Translations.set_locale(user_lang)
 
   def login_button_click(self, **event_args):
     user = anvil.users.login_with_form(allow_cancel=True, show_signup_option=False, allow_remembered=True)
