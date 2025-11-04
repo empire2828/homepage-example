e8a02a3877ca86deb99d2063276e6f8de43414cc
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-04 21:50:23 +0000

    Created home_template and home; Edited settings; Deleted Form1

--- client_code/home_template/__init__.py
+++ client_code/home_template/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import home_templateTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,7 +9,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class home_template(home_templateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
