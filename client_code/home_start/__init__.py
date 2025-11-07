7bf6186ea583d95f43e4e9304ee322e162695dfc
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-07 19:19:57 +0000

    Created home_de, home_en and home_start; Edited settings; Deleted home

---
+++ client_code/home_start/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import home_startTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+from anvil.tables import app_tables
+
+class home_start(home_startTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    user_locale = anvil.js.window.navigator.language
+    if user_locale.lower().startswith("de"):
+      open_form('home_de')
+    else:
+      open_form('home_en')
+    print(user_locale)
