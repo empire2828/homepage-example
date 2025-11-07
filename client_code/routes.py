ec48203433b5a9ab1ed7f4b5b01ab6fa8b8f86d5
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-07 20:03:41 +0000

    Edited routes

--- client_code/routes.py
+++ client_code/routes.py
@@ -4,9 +4,13 @@ from routing.router import Route #, debug_logging
 
 class homepage_route(Route):
     path = "/"
-    form = "home"
+    form = "home_start"
     #cache_form = True
     #cache_data = True
+
+class home_de(Route):
+  path = "/de"
+  form = "home_de"
 
 class data_protection_route(Route):
     path = "/data_protection"
