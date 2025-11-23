5dc38a16bb3c6ec98b2f815d3405545b725811ad
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-23 19:12:36 +0000

    Edited blog.cancellations_de, blog.cancellations_en, blog.stly_de, blog.stly_en and 2 more

--- client_code/globals.py
+++ client_code/globals.py
@@ -1,18 +1,26 @@
+from anvil import *
 import anvil.server
-#from routing import router
-#import anvil.google.auth, anvil.google.drive
-#from anvil.google.drive import app_files
-#import anvil.users
-#import anvil.tables as tables
-#import anvil.tables.query as q
-#from anvil.tables import app_tables
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+import anvil.users
 
 user_has_subscription = False
 user_email = None
 request_count = 0
 current_user = None
 
-def say_hello():
-  print("Hello, world")
+def testen_button_click():
+  global current_user, user_has_subscription, request_count
+  user = anvil.users.signup_with_form(allow_cancel=True)
+  if user:
+    anvil.server.call("create_supabase_key")
+    anvil.server.call("send_registration_notification", user["email"])
+    # Layout Template öffnen
+    current_user = user
+    request_count = 0
+    user_has_subscription= False
+    layout_form = open_form("layout_template")
+    # Dashboard automatisch laden
+    layout_form.reset_links()
+    layout_form.dashboard_navigation_link.selected = True   
+    multiframe_form = layout_form.open_multiframe_form()
+    multiframe_form.lade_und_zeige_iframe(0)  # Index 0 = Dashboard
+    # Navigation Link als aktiv markieren
