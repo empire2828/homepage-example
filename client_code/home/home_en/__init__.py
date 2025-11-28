3b08ef8ea94a29fe8364e25a23ce5f07a016a1aa
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-28 17:32:05 +0000

    Edited home.home_en and home.home_de

--- client_code/home/home_en/__init__.py
+++ client_code/home/home_en/__init__.py
@@ -15,20 +15,16 @@ class home_en(home_enTemplate):
   
   def testen_button_click(self, **event_args):
     user = anvil.users.signup_with_form(allow_cancel=True)
-    if user:
-      anvil.server.call('create_supabase_key')
-      anvil.server.call('send_registration_notification', user['email'])
-      # Layout Template öffnen
+    if user:     
+      # init variables
       globals.current_user = user
       globals.request_count = 0
       globals.user_has_subscription= False
-      layout_form = open_form('layout_template')
-      # Dashboard automatisch laden
-      layout_form.reset_links()
-      layout_form.dashboard_navigation_link.selected = True   
-      multiframe_form = layout_form.open_multiframe_form()
-      multiframe_form.lade_und_zeige_iframe(0)  # Index 0 = Dashboard
-      # in alle Blogs Code ändern wenn hier geändert!
+      anvil.server.call_s("create_supabase_key")
+
+      open_form('channel_manager_connect')
+
+      anvil.server.call_s("send_registration_notification", user["email"])
     pass
   
   def impressum_link_click(self, **event_args):
