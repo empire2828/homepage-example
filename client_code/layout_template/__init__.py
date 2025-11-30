9f514ffd6f7cb511e7b198bf5a3679ff35d43a05
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-11-30 18:03:31 +0000

    Edited channel_manager_connect, layout_template, help and my_account

--- client_code/layout_template/__init__.py
+++ client_code/layout_template/__init__.py
@@ -54,7 +54,7 @@ class layout_template(layout_templateTemplate):
     self.detailed_bookings_navigation_link.selected = False
     self.long_trends_navigation_link.selected = False
     self.connect_navigation_link.selected = False
-    self.knowledge_hub_link.selected = False
+    self.help_link.selected = False
     self.my_account_navigation_link.selected = False
     self.upgrade_navigation_link.selected = False
 
@@ -142,12 +142,12 @@ class layout_template(layout_templateTemplate):
     self.reset_links()
     self.my_account_navigation_link.selected = True
 
-  def knowledge_hub_link_click(self, **event_args):
+  def help_link_click(self, **event_args):
     globals.current_multiframe_instance = None
     globals.multiframe_open = False
-    open_form('knowledge_hub')
+    open_form('help')
     self.reset_links()
-    self.knowledge_hub_link.selected = True
+    self.help_link.selected = True
 
   def upgrade_navigation_link_click(self, **event_args):
     globals.current_multiframe_instance = None
