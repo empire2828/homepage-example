2d01dbae06157514dd42e261ebb31d0932240403
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-12-02 13:01:03 +0000

    Edited scheduled_tasks

--- server_code/scheduled_tasks.py
+++ server_code/scheduled_tasks.py
@@ -3,7 +3,6 @@ import anvil.http
 from anvil.tables import app_tables
 import time
 from datetime import datetime, timedelta, timezone
-from supabase import create_client
 from servermain import get_bigquery_client
 from google.cloud import bigquery
 
