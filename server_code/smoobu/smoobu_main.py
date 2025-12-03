7f98ce2594f00716be7494242942e5eb9e47fa6c
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-12-03 07:52:52 +0000

    Edited smoobu.smoobu_main

--- server_code/smoobu/smoobu_main.py
+++ server_code/smoobu/smoobu_main.py
@@ -29,7 +29,7 @@ def get_price_elements(reservation_id, headers, wait_for_sync=False):
       price_elements = price_elements_response.json().get("priceElements", [])
 
       print("[smoobu main] get_price_elements Reservation-ID",reservation_id," price_elements:",price_elements)
-      log(price_elements,reservation_id,"[smoobu_main] get_price_elements")
+      #log(price_elements,reservation_id,"[smoobu_main] get_price_elements")
       
       has_addon = any(pe.get('type') == 'addon' for pe in price_elements)
       has_cleaningFee = any(pe.get('type') == 'cleaningFee' for pe in price_elements)
