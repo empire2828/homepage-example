45eb111825d0c88b65028100ee199e450d4b9390
dirk.klemer@gmail.com <dirk.klemer@gmail.com>
2025-12-06 21:39:59 +0000

    Edited blog.ota_markup_de; Deleted home.ota_markup_de

--- client_code/blog/ota_markup_de/__init__.py
+++ client_code/blog/ota_markup_de/__init__.py
@@ -1,28 +1,29 @@
 from ._anvil_designer import ota_markup_deTemplate
 from anvil import *
+import anvil.server
+from routing import router
+import m3.components as m3
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from routing import router
-
-# import m3.components as m3
-import anvil.server
-
-# import anvil.google.auth, anvil.google.drive
-import anvil.users
-# import anvil.tables as tables
-# import anvil.tables.query as q
-# from anvil.tables import app_tables
-# from ... import globals
-
 
 class ota_markup_de(ota_markup_deTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def learn_more_button_click(self, **event_args):
-    open_form("home.home_de")
+    # Any code you write here will run before the form opens.
+
+  def ota_commission_text_box_pressed_enter(self, **event_args):
+    self.calculate_ota_markup()
     pass
+
+  def calculate_ota_markup(self, **event_args):
+    ota_comm = int(self.ota_commission_text_box.text)
+    ota_markup_result = (100 / (100 - ota_comm) - 1) * 100
+    ota_markup_result_rounded = round(ota_markup_result, 2)
+    self.ota_markup_result_body.text = f"{ota_markup_result_rounded:.2f} %"
+    return 
